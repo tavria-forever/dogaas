c4b6b4412c750928823ff3827417a21515e43892
Nickolay Ilchenko <yndx.tavria@yandex.ru>
2022-04-03 18:55:26 +0300

    Деплой через yappa

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dogaas
 
-Работает на python@3.7.12
+**n.b** Работает на python=3.8.13
 
 1. Создать и активировать venv
 
@@ -18,5 +18,19 @@ pip install -r requirements.txt
 3. Локальный запуск
 
 ```bash
-uvicorn main:app --reload
+python main.py
+```
+
+## Деплой
+
+1. Настроить `yappa`, если раньше этого не делали, если уже, сразу переходим к шагу 2.
+
+```bash
+yappa setup
+```
+
+2. Загрузка в яндекс.облако
+
+```bash
+yappa deploy
 ```
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import logging
+import tempfile
 
+import uvicorn
 import requests
-from fastapi import FastAPI, Response, Request
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -25,10 +28,12 @@ async def index(request: Request):
         logger.info(f'Получили ответ API: {response.text}')
         response = response.json()
         image_url = response.get('message')
-        image_response = requests.get(image_url)
-        image_response.raise_for_status()
-        logger.info('Загрузили картинку из dog.ceo')
-        return Response(content=image_response.content, media_type='image/jpg')
+        with tempfile.NamedTemporaryFile(mode='w+b', suffix='.jpeg') as outfile:
+            logger.info('Загрузили картинку из dog.ceo')
+            content = requests.get(image_url).content
+            outfile.write(content)
+            return FileResponse(outfile.name, media_type='image/jpeg')
+
     except Exception as error:
         message = f'Сбой в работе программы: {error}'
         logger.error(message)
@@ -36,3 +41,7 @@ async def index(request: Request):
         return {
             'error': message,
         }
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info',
+                reload=True)
