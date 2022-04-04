2b3c73435a6eb90d62a716a6f4d935cfc09e5e56
Nickolay Ilchenko <yndx.tavria@yandex.ru>
2022-04-04 19:34:53 +0300

    Обновление yappa=0.4.20

--- main.py
+++ main.py
@@ -12,35 +12,35 @@ handler = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
 app = FastAPI()
+
+ENDPOINT = 'https://dog.ceo/api/breeds/image/random'
 
 
 @app.get('/')
 async def index(request: Request):
     """Метод возвращает случайную картинку из dog.ceo."""
-    endpoint = 'https://dog.ceo/api/breeds/image/random'
-
     logger.info(f'Хост: {request.client.host}')
 
     try:
-        response = requests.get(endpoint)
+        response = requests.get(ENDPOINT)
         response.raise_for_status()
         logger.info(f'Получили ответ API: {response.text}')
         response = response.json()
         image_url = response.get('message')
-        with tempfile.NamedTemporaryFile(mode='w+b', suffix='.jpeg') as outfile:
-            logger.info('Загрузили картинку из dog.ceo')
-            content = requests.get(image_url).content
-            outfile.write(content)
-            return FileResponse(outfile.name, media_type='image/jpeg')
+        logger.info('Загрузили картинку из dog.ceo')
+        content = requests.get(image_url).content
+
+        with tempfile.NamedTemporaryFile(mode='wb', delete=False) as jpg:
+            jpg.write(content)
+            
+        return FileResponse(jpg.name, media_type='image/jpeg')
 
     except Exception as error:
         message = f'Сбой в работе программы: {error}'
         logger.error(message)
-
-        return {
-            'error': message,
-        }
+        return {'error': message}
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info',
